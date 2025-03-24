afd35eb5c6ddc92379a6a13fbfd260e04c2120b6
svej123 <artemposvezhinskiy@gmail.com>
2025-03-24 18:35:41 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func BinarySearch(array []int, needfull int) int {
+func BS(array []int, needfull int) int {
 	left := 0
 	right := len(array) - 1
 
@@ -22,7 +22,7 @@ func BinarySearch(array []int, needfull int) int {
 func main() {
 	array := []int{14, 20, 25, 42}
 	A := 42
-	index := BinarySearch(array, A)
+	index := BS(array, A)
 
 	if index != -1 {
 		fmt.Printf("Число %d найдено в массиве на индексе %d (порядковый номер: %d)\n", A, index, index+1)
