8690ec9f39cd645f4c44f67dc164f3b1c20ae6f3
sosal <sosal@sosals-MacBook-Air.local>
2025-03-24 18:27:26 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func BinarySearch(array []int, needfull int) int {
+	left := 0
+	right := len(array) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if array[mid] == needfull {
+			return mid
+		} else if array[mid] > needfull {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	array := []int{14, 20, 25, 42}
+	A := 42
+	index := BinarySearch(array, A)
+
+	if index != -1 {
+		fmt.Printf("Число %d найдено в массиве на индексе %d (порядковый номер: %d)\n", A, index, index+1)
+	} else {
+		fmt.Println("Число не найдено")
+	}
+}
